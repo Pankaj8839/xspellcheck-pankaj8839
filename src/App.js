647d2399ac052602293cf03caf83377022794e7b
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-04 19:46:43 +0530

    please pass one go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const customDictionary = {
+  teh: "the",
+  wrok: "work",
+  fot: "for",
+  exampl: "example"
+};
 
 function App() {
+  const [text, setText] = useState("");
+  const [suggestedText, setSuggestedText] = useState("");
+
+  const handleChange = (e) => {
+    let value=e.target.value;
+    setText(value);
+    let words = value.toLowerCase().split(" ");
+    let corrections=words.find((word) => customDictionary[word]!==undefined);
+    let suggusedWord = corrections ? customDictionary[corrections] : "";
+    setSuggestedText(suggusedWord); 
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea placeholder="Enter text..." style={{ width: "45vw", height: "200px" }} value={text} onChange={(e) => handleChange(e)}></textarea>
+     {suggestedText!=="" && <p>Did you mean: {suggestedText}?</p>}
     </div>
   );
 }
